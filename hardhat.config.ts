e4ee1ed9566abe2af6507c44c5caef8365cade80
刘鹤翔 <liuhexiang@bytedance.com>
2023-11-27 19:11:05 +0800

    feat: update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,39 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import 'hardhat-deploy';
+import 'hardhat-deploy-ethers';
+import '@nomicfoundation/hardhat-ethers';
+import '@nomicfoundation/hardhat-toolbox';
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: '0.8.19',
+  networks: {
+    localhost: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY!],
+      chainId: 11155111,
+    },
+    mainnet: {
+      url: process.env.MAINNET_RPC_URL,
+      accounts: [process.env.MAINNET_PRIVATE_KEY!],
+      chainId: 1,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+    },
+    user: {
+      default: 1,
+    },
+  },
 };
 
 export default config;
